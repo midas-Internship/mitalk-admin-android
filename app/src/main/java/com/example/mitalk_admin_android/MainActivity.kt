648e5b85945916710d06787fcb558dccee382040
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-16 17:23:53 +0900

    feat: navigation setting

--- app/src/main/java/com/example/mitalk_admin_android/MainActivity.kt
+++ app/src/main/java/com/example/mitalk_admin_android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mitalk_admin_android
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mitalk_admin_android.sample.ui.SampleScreen
+import com.example.mitalk_admin_android.sample.ui.admin.AdminMainScreen
 import com.example.mitalk_admin_android.ui.LoginScreen
 import com.example.mitalk_admin_android.util.theme.MitalkadminandroidTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,8 +28,27 @@ class MainActivity : ComponentActivity() {
         setContent {
             MitalkadminandroidTheme {
                 val navController = rememberNavController()
-                LoginScreen(navController)
+                BaseApp(navController)
             }
         }
     }
+}
+
+@Composable
+fun BaseApp(navController: NavHostController) {
+
+    NavHost(navController = navController, startDestination = AppNavigationItem.Login.route) {
+        composable(AppNavigationItem.Login.route) {
+            LoginScreen(navController = navController)
+        }
+        composable(AppNavigationItem.AdminMain.route) {
+            AdminMainScreen(navController = navController)
+        }
+    }
+}
+
+sealed class AppNavigationItem(val route: String) {
+    object Login : AppNavigationItem("Login")
+
+    object AdminMain : AppNavigationItem("AdminMain")
 }
