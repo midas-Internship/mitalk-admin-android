9436d75791051700fa46625c2d49648dd4f1a79a
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-17 14:58:18 +0900

    feat: textfield focus 처리

--- app/src/main/java/com/example/mitalk_admin_android/ui/admin/header/AdminHeader.kt
+++ app/src/main/java/com/example/mitalk_admin_android/ui/admin/header/AdminHeader.kt
@@ -1,6 +1,7 @@
 package com.example.mitalk_admin_android.ui.admin.header
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -8,9 +9,12 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,11 +28,11 @@ import com.example.mitalk_admin_android.util.theme.Regular14NO
 fun AdminHeader(
     navController: NavController,
     title: String,
+    findOn: Boolean = false,
     findEnabled: Boolean = true,
     findOnPressed: (String) -> Unit,
-    focusRequester: FocusRequester
+    findOnRequest: () -> Unit
 ) {
-    var findOn by remember { mutableStateOf(false) }
     var findText by remember { mutableStateOf("") }
 
     Row(
@@ -52,8 +56,11 @@ fun AdminHeader(
             )
         }
 
-        if (findOn) {
-          
+        if (findOn || findText.isNotEmpty()) {
+            TextField(
+                value = findText,
+                onValueChange = { findText = it },
+            )
         } else {
             Regular14NO(text = title)
         }
@@ -61,11 +68,8 @@ fun AdminHeader(
         if (findEnabled) {
             IconButton(
                 onClick = {
-                    if (findOn) {
-
-                    } else {
-                        findOn = true
-                    }
+                    if (findOn) { findOnPressed(findText) }
+                    else { findOnRequest() }
                 },
                 modifier = Modifier
                     .fillMaxWidth()
@@ -80,9 +84,38 @@ fun AdminHeader(
     }
 }
 
+fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            doOnClear()
+            focusManager.clearFocus()
+        })
+    }
+}
+
 @Composable
 @Preview(showBackground = true)
 fun ShowAdminHeader() {
     val navController = rememberNavController()
-    AdminHeader(navController = navController, title = "통계보기")
+    val focusManger = LocalFocusManager.current
+    var findOn by remember { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .addFocusCleaner(focusManger) {
+                findOn = false
+            }
+    ) {
+        AdminHeader(
+            navController = navController,
+            title = "통계보기",
+            findOn = findOn,
+            findOnPressed = {},
+            findOnRequest = {
+                findOn = true
+            }
+        )
+    }
+
 }
