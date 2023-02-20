016521bcab6115ebccae739c6af0f6c94bed0a3c
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-20 13:28:37 +0900

    feat: apicall

--- data/src/main/java/com/example/data/remote/datasource/admin/RemoteGetMessageRecordDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/admin/RemoteGetMessageRecordDataSourceImpl.kt
@@ -2,12 +2,14 @@ package com.example.data.remote.datasource.admin
 
 import com.example.data.remote.api.AdminApi
 import com.example.data.remote.response.admin.toEntity
+import com.example.data.remote.util.miTalkApiCall
 import com.example.domain.entity.admin.GetMessageRecordEntity
 import javax.inject.Inject
 
 class RemoteGetMessageRecordDataSourceImpl @Inject constructor(
     private val adminApi: AdminApi
 ): RemoteGetMessageRecordDataSource {
-    override suspend fun getMessageRecordList(): List<GetMessageRecordEntity> =
+    override suspend fun getMessageRecordList(): List<GetMessageRecordEntity> = miTalkApiCall {
         adminApi.getRecordList().map { it.toEntity() }
+    }
 }
