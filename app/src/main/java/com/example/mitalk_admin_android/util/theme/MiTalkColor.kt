dc3849e73b747b1fec10a2c27a73f796ca43f5ba
백승민 <81063667+100Seung-Min@users.noreply.github.com>
2023-02-20 20:36:17 +0900

    feat: 기본 컬러 추가

--- app/src/main/java/com/example/mitalk_admin_android/util/theme/MiTalkColor.kt
+++ app/src/main/java/com/example/mitalk_admin_android/util/theme/MiTalkColor.kt
@@ -6,4 +6,7 @@ object MiTalkColor {
     val Black = Color(0xFF000000)
     val White = Color(0xFFFFFFFF)
     val Gray = Color(0xFFF8F8F8)
+    val MainBlue = Color(0xFF7B78FF)
+    val MainBrown = Color(0xFFD2947A)
+    val MainGreen = Color(0xFF89CEA9)
 }
