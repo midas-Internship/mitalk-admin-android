99f803fce116ab2a6e5a508e1fe746709c5faeb0
MIDASIT\kjh1007 <kimjjun1007@gmail.com>
2023-02-20 10:50:24 +0900

    feat: get user list api

--- data/src/main/java/com/example/data/remote/datasource/admin/RemoteGetUserListDataSourceImpl.kt
+++ data/src/main/java/com/example/data/remote/datasource/admin/RemoteGetUserListDataSourceImpl.kt
@@ -1,10 +1,13 @@
 package com.example.data.remote.datasource.admin
 
+import com.example.data.remote.api.AdminApi
+import com.example.data.remote.response.toEntity
 import com.example.domain.entity.admin.GetAdminUserCareEntity
 import javax.inject.Inject
 
 class RemoteGetUserListDataSourceImpl @Inject constructor(
-    
+    private val adminApi: AdminApi
 ): RemoteGetUserListDataSource {
-    override suspend fun getUserList(): List<GetAdminUserCareEntity> = listOf()
+    override suspend fun getUserList(): List<GetAdminUserCareEntity> =
+        adminApi.getUserList().map { it.toEntity() }
 }
