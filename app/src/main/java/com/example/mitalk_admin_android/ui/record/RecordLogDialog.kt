3c5ab360da375e79c8561ed58f0f8575a2b87a42
MIDASIT\bsm0417 <bsmin0417@naver.com>
2023-02-22 13:16:42 +0900

    feat: 채팅 데이터 로그 보는 다이얼로그 구현

--- app/src/main/java/com/example/mitalk_admin_android/ui/record/RecordLogDialog.kt
+++ app/src/main/java/com/example/mitalk_admin_android/ui/record/RecordLogDialog.kt
@@ -1,12 +1,16 @@
 package com.example.mitalk_admin_android.ui.record
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.example.mitalk_admin_android.mvi.RecordDetailState
+import com.example.mitalk_admin_android.util.theme.MiTalkColor
 import com.example.mitalk_admin_android.util.theme.Regular12NO
 import com.example.mitalk_admin_android.util.toChatTime
 
@@ -18,11 +22,26 @@ fun RecordLogDialog(
 ) {
     if (visible) {
         Dialog(onDismissRequest = onDismissRequest) {
-            LazyColumn {
+            LazyColumn(
+                modifier = Modifier
+                    .background(
+                        color = MiTalkColor.White,
+                        shape = RoundedCornerShape(5.dp)
+                    )
+                    .padding(horizontal = 15.dp, vertical = 5.dp)
+            ) {
                 items(itemList) {
-                    Row(horizontalArrangement = Arrangement.SpaceBetween) {
-                        Regular12NO(text = it.message)
-                        Regular12NO(text = it.time.toChatTime())
+                    Column {
+                        Spacer(modifier = Modifier.height(5.dp))
+                        Row(
+                            modifier = Modifier
+                                .width(230.dp)
+                                .heightIn(max = 200.dp)
+                        ) {
+                            Regular12NO(text = it.message)
+                            Regular12NO(text = it.time.toChatTime())
+                        }
+                        Spacer(modifier = Modifier.height(5.dp))
                     }
                 }
             }
